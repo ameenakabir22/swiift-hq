661431befe8046259c298edc4bcbdf2aa4d7cb2c
Amina_Kabir <ameenakabir22@gmail.com>
2024-01-01 18:27:43 +0000

    i created the splash screen page and added the needed assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:swiift/pages/splashscreen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Swiift());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Swiift extends StatelessWidget {
+  const Swiift({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
