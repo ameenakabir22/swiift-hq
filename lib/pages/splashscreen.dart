50e151e39663bdaf9a46a96fcdb41ca431e7638f
Amina_Kabir <ameenakabir22@gmail.com>
2024-01-02 16:29:53 +0000

    i created the splash screen

--- lib/pages/splashscreen.dart
+++ lib/pages/splashscreen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Center(
+            child: Image.asset(
+      'assets/swiiftlogo.png',
+      height: 200,
+      width: 200,
+    )));
   }
 }
