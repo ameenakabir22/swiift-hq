6cfe87e49f48fc80fa48b4c3184956ff7be51755
Amina_Kabir <ameenakabir22@gmail.com>
2024-01-08 12:35:10 +0000

    still on the onboarding page

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -18,31 +18,40 @@ class _OnboardingState extends State<Onboarding> {
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Text(
-                'Connecting Relaible Delivery Partners To Your Doorsteps, Anytime, Anywhere'),
+              'Connecting Reliable Delivery Partners To Your Doorsteps, Anytime, Anywhere',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
+            ),
           ),
-          Image.asset(
-            'assets/onboardingimg.jpeg',
+          Transform.translate(
+            offset: Offset(0.0, -60.0),
+            child: Image.asset(
+              'assets/onboardingimg.jpeg',
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20, left: 20),
-            child: TextButton(
-              onPressed: () {},
-              child: Text('Get Started'),
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF50E3C2), // Background color
-                onPrimary: Colors.white, // Text color
-                padding: EdgeInsets.symmetric(
-                    vertical: 16.0, horizontal: 70), // Vertical padding
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0), // Border radius
+          Transform.translate(
+            offset: Offset(0.0, -80.0),
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20, left: 20),
+              child: TextButton(
+                onPressed: () {},
+                child: Text('Get Started'),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF50E3C2), // Background color
+                  onPrimary: Colors.white, // Text color
+                  padding: EdgeInsets.symmetric(
+                    vertical: 16.0,
+                    horizontal: 70,
+                  ), // Vertical padding
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0), // Border radius
+                  ),
                 ),
               ),
             ),
           ),
-          SizedBox(
-            height: 50.0,
-          ),
-          Text('Already have an account? Signup'),
+          Transform.translate(
+              offset: Offset(0.0, -50.0),
+              child: Text('Already have an account? Signup'))
         ],
       ),
     );
