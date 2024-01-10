ec00a826af7d2b48c40b7352f721d0b8a40f1367
Amina_Kabir <ameenakabir22@gmail.com>
2024-01-10 19:42:13 +0000

    i added a return button to the third onboarding page

--- lib/pages/onboarding3.dart
+++ lib/pages/onboarding3.dart
@@ -10,6 +10,22 @@ class Onboarding3 extends StatefulWidget {
 class _Onboarding3State extends State<Onboarding3> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            // Navigate back to the previous page when the back arrow is pressed
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
