d8256020a461e064fe1a6089fce7e4687d5ce434
Amina_Kabir <ameenakabir22@gmail.com>
2024-02-08 14:52:30 +0000

    i made new changes to the onboarding page

--- lib/pages/onboarding4.dart
+++ lib/pages/onboarding4.dart
@@ -18,12 +18,13 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   // keep track of whether we are on the last page or not.
   bool onLastPage = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          //page view
+          // Page view
           PageView(
             controller: _controller,
             onPageChanged: (index) {
@@ -34,31 +35,76 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             children: [Onboarding(), Onboarding2(), Onboarding3()],
           ),
 
+          // Dot indicators
+          Transform.translate(
+            offset: Offset(20.0, 585.0),
+            child: SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: const ExpandingDotsEffect(
+                activeDotColor: Colors.black,
+                dotHeight: 6,
+              ),
+            ),
+          ),
+
+          // Positioned widget for UI elements
           Container(
             alignment: Alignment(0, 0.75),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                // skip button
+                // Skip button
                 GestureDetector(
                     onTap: () {
                       _controller.jumpToPage(2);
                     },
-                    child: Text('Skip')),
+                    child: Material(
+                      color: Colors.black, // Set background color to black
+                      borderRadius: BorderRadius.circular(
+                          50), // Set a large value for complete roundness
+                      child: InkWell(
+                        onTap: () {
+                          // Add your onTap callback here
+                        },
+                        borderRadius: BorderRadius.circular(
+                            50), // Set the same value for complete roundness
+                        child: Container(
+                          width: 100, // Set a fixed width for the button
+                          height: 100, // Set a fixed height for the button
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Add your onTap callback here
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary:
+                                  Colors.black, // Set background color to black
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    50), // Set a large value for complete roundness
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
 
-                // dot indicators
-                SmoothPageIndicator(controller: _controller, count: 3),
-
-                // next or done button
-                onLastPage
-                    ? GestureDetector(
+                    // Next or Done button
+                    /*onLastPage
+                     ? GestureDetector(
                         onTap: () {
                           Navigator.push(context,
                               MaterialPageRoute(builder: (context) {
                             return SignUpOptions();
                           }));
                         },
-                        child: Text('Done'))
+                        child: Text('Done'),
+                      )
                     : GestureDetector(
                         onTap: () {
                           _controller.nextPage(
@@ -66,10 +112,12 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                             curve: Curves.easeIn,
                           );
                         },
-                        child: Text('Next')),
+                        child: Text('Next'),
+                      ), */
+                    )
               ],
             ),
-          )
+          ),
         ],
       ),
     );
