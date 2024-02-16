0971025ab3b73e6dd35a26e813bf87420059d436
Amina_Kabir <ameenakabir22@gmail.com>
2024-02-16 20:22:32 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:swiift/pages/onboarding4.dart';
 import 'package:swiift/pages/splashscreen.dart';
 
 void main() => runApp(Swiift());
