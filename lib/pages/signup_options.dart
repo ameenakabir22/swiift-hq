5e9d4e0e7237ffd303177a3d3f90a24da8945cbe
Amina_Kabir <ameenakabir22@gmail.com>
2024-02-29 20:39:20 +0000

    i adjusted the sign up options

--- lib/pages/signup_options.dart
+++ lib/pages/signup_options.dart
@@ -9,22 +9,21 @@ class SignUpOptions extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_ios_new_rounded,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            // Navigate back to the previous page when the back arrow is pressed
-            Navigator.pop(context);
-          },
-        ),
-      ),
       body: ListView(
         children: [
+          Padding(
+            padding: EdgeInsets.only(right: 250.w, top: 20.h),
+            child: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                // Navigate back to the previous page when the back arrow is pressed
+                Navigator.pop(context);
+              },
+            ),
+          ),
           GestureDetector(
             onTap: () {
               // Navigate to the SignUp page when either the text or icon is tapped
